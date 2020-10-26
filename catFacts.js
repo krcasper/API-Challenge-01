0977b63dcbab0c704c03486505057c07b9ceb1d8
Kelly Casper <kellycasper@Kellys-MacBook-Air.local>
2020-10-26 08:34:01 -0400

    first commit

---
+++ catFacts.js
@@ -0,0 +1,46 @@
+const baseURL = 'https://cat-fact.herokuapp.com';
+const wrapper = document.querySelector('.wrapper');
+
+// fetch(`${baseURL}/facts`)
+//   .then(function (result) {
+//     return result.json();
+//   })
+//   .then(function (json) {
+//     console.log(json)
+//     displayResults(json)
+//   });
+
+//   function displayResults(facts) {
+//       for (i = 0; i < 10; i++) {
+//           console.log(facts.all[i].text);
+//           let para = document.createElement('p');
+//           para.textContent = `${facts.all[i].text}`;
+//           section.appendChild(para);
+//       }
+//   }
+
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+
+fetch(`${baseURL}/facts/random?amount=10`)
+  .then(function (result) {
+    return result.json();
+  })
+  .then(function (json) {
+    console.log(json)
+    displayResults(json)
+  });
+
+  function displayResults(facts) {
+      for (i = 0; i < facts.length; i++) {
+          console.log(facts[i].text);
+          let para = document.createElement('p');
+          para.setAttribute('class', 'fact');
+          para.textContent = `${facts[i].text}`;
+          wrapper.appendChild(para);
+      }
+  }
+  
+
